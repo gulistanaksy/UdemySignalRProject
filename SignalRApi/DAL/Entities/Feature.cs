5471605f57b3b3f67d9bb1faa5d2af17ef8c68f1
Gülistan Aksoy <gulistanaksoy87@gmail.com>
2025-09-04 14:55:31 +0300

    Add project files.

---
+++ SignalRApi/DAL/Entities/Feature.cs
@@ -0,0 +1,14 @@
+﻿namespace SignalRApi.DAL.Entities
+{
+    // öne çıkarılanlar
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string Title1 { get; set; }
+        public string Description1 { get; set; }
+        public string Title2 { get; set; }
+        public string Description2 { get; set; }
+        public string Title3 { get; set; }
+        public string Description3 { get; set; }
+    }
+}
