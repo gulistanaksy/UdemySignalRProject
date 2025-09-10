6a5c50087191e97a35b0e1b5ec35b2e61d71fcf6
Gülistan Aksoy <gulistanaksoy87@gmail.com>
2025-09-10 10:59:36 +0300

    Category Controller yapıldı

---
+++ SignalRApi/Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.CategoryDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var value = _mapper.Map<List<ResultCategoryDto>>(_categoryService.TGetListAll());
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var value = _mapper.Map<Category>(createCategoryDto);
+            _categoryService.TAdd(value);
+            return Ok("Kategori eklendi.");
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _categoryService.TGetById(id);
+            _categoryService.TDelete(value);
+            return Ok("Kategori silindi");
+        }
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _categoryService.TGetById(id);
+            return Ok(value);
+        }
+        [HttpPut] 
+        public IActionResult UpdateCagtegor(UpdateCategoryDto updateCategoryDto)
+        {
+            var value = _mapper.Map<Category>(updateCategoryDto);
+            _categoryService.TUpdate(value);
+            return Ok("Kategori güncellendi.");
+        }
+    }
+}
